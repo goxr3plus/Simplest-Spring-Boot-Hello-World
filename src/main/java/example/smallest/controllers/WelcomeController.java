41ea55513daa92ca807ecb91effb3af38a156df7
Xianyao Chen <owenchenxy@gmail.com>
2023-10-10 22:50:59 +0800

    add image uri to response text

--- src/main/java/example/smallest/controllers/WelcomeController.java
+++ src/main/java/example/smallest/controllers/WelcomeController.java
@@ -16,6 +16,6 @@ public class WelcomeController {
 		
 		
 		
-		return "Hello FUCKING World!!!"; //"application/json" mean this is a text not a redirect
+		return "Hello FUCKING World!!!\n {{imageUri}}"; //"application/json" mean this is a text not a redirect
 	}
 }
